4343bfff93140cf2bbd5821a1475c23c8c34434e
Juan Marcet <juanmarcet06@gmail.com>
2025-05-29 21:05:05 -0500

    añadí funciones para sacar la tabla de multiplicar y crear elementos HTML que tienen padre

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-function arreglo(lista, fruta){
-    lista.push(fruta);
+function tablaMultiplicar(numero) {
+    const padre = document.getElementById("resultadoMultiplicar");
+
+    for (let i = 1; i <= 12; i++) {
+        const resultado = parseFloat((numero * i).toFixed(2)); // to fixed(2) para limitar a 2 decimales
+        generarElementoHijo(padre, "li", `${numero} x ${i} = ${resultado}`);
+        console.log(`${numero} x ${i} = ${resultado}`);
+    }
 }
 
-let frutas = ["manzana", "pera", "naranja"];
-console.log(frutas);
-arreglo(frutas, "uva"); // llamda correccta a la funcion
-console.log(frutas); // ["manzana", "pera", "naranja", "uva"]
+
+function generarElementoHijo(padre, elementoCrear, contenido){
+    const nuevoElemento = document.createElement(elementoCrear);
+    nuevoElemento.textContent = contenido;
+    padre.appendChild(nuevoElemento);
+}
+
+const btnMultiplicar = document.getElementById("btnMultiplicar");
+btnMultiplicar.addEventListener("click", function () {
+    console.log("click");
+    const numero = parseFloat(document.getElementById("numeroMultiplicar").value);
+    if (isNaN(numero)) {
+        alert("Por favor, ingrese un número válido.");
+        return;
+    } else {
+        // console.log("pasó");
+        document.getElementById("resultadoMultiplicar").innerHTML = ""; // Limpiar resultados anteriores
+        document.getElementById("numeroMultiplicar").value = ""; // Limpiar el campo de entrada
+        document.getElementById("numeroMultiplicar").focus(); // Volver a enfocar el campo de entrada
+        document.getElementById("tituloResultado").textContent = `Tabla de multiplicar del ${numero}`;
+        tablaMultiplicar(numero);
+    }
+});
