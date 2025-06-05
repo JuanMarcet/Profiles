63445fbc01bfd36fc837685af800cc490a57c525
Juan Marcet <juanmarcet06@gmail.com>
2025-06-04 20:04:27 -0500

    aplicando responsividad a calculos matemáticos y separar Styles

--- script.js
+++ script.js
@@ -132,3 +132,88 @@ btnMultiplicar.addEventListener("click", function () {
         tablaMultiplicar(numero);
     }
 });
+
+
+// funcion de agregar nombres a la lista
+// Obtener elementos del DOM
+    const agregarBtn = document.getElementById("agregarBtn");
+    const nombreInput = document.getElementById("nombreInput");
+    const listaNombres = document.getElementById("listaNombres");
+    const mensaje = document.getElementById("mensaje");
+
+    // Agregar evento al botón al dar enter
+nombreInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            agregar();
+        }
+    });
+
+
+    function agregar() {
+    const nombre = nombreInput.value.trim();
+
+    if (nombre === "") {
+        mensaje.textContent = "⚠️ Por favor, escribe un nombre.";
+        return;
+    }
+    mensaje.textContent = "";
+
+    // Crear elemento <li>
+    const li = document.createElement("li");
+    li.textContent = nombre;
+
+    // Crear un <div> para contener los botones
+    const div = document.createElement("div");
+    div.classList.add("contenedorBotones");
+
+    // Crear botón de eliminar
+    const eliminarBtn = document.createElement("button");
+    eliminarBtn.textContent = "Eliminar";
+    eliminarBtn.classList.add("botonlista" , "eliminar");
+
+        // Crear botón de editar
+    const editarBtn = document.createElement("button");
+    editarBtn.textContent = "Editar";
+    editarBtn.classList.add("botonlista" , "editar");
+    
+    // Agregar funcionalidad para eliminar
+    eliminarBtn.addEventListener("click", () => {
+        eliminar(li);
+    });
+
+    // Agregar funcionalidad para editar
+    editarBtn.addEventListener("click", () => {
+        editar(li);
+    });
+
+    // agregar los botones al <div>
+    div.appendChild(eliminarBtn);
+    div.appendChild(editarBtn);
+
+    // agregar el <div> al <li>
+    li.appendChild(div);
+
+    // Agregar <li> a la lista
+    listaNombres.appendChild(li);
+
+    // Limpiar el input
+    nombreInput.value = "";
+    nombreInput.focus();
+}
+
+function eliminar(li) {
+    if (confirm("¿Estás seguro de que deseas eliminar este nombre?")) {
+        li.remove();
+    } else {
+        alert("Operación cancelada");
+    }
+}
+
+function editar(li) {
+    const nuevoNombre = prompt("Introduce el nuevo nombre:", li.firstChild.textContent);
+    if (nuevoNombre !== null && nuevoNombre.trim() !== "") {
+        li.firstChild.textContent = nuevoNombre;
+    } else {
+        alert("Operación cancelada");
+    }
+}
